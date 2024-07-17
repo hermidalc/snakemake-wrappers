429593f67781310d8bbb94cff84c1028be694d13
Leandro Hermida <softdev@leandrohermida.com>
2024-07-17 18:29:19 -0400

    Allow positional FASTQ inputs too

--- bio/bbmap/readlength/wrapper.py
+++ bio/bbmap/readlength/wrapper.py
@@ -6,10 +6,10 @@ from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-fq1 = snakemake.input.get("fq1")
-assert fq1 is not None, "input: fq1 is a required input parameter"
+fq1 = snakemake.input.get("fq1") or snakemake.input[0]
+assert fq1 is not None, "input: fq1 is a required named or positional input parameter"
 
-fq2 = snakemake.input.get("fq2")
+fq2 = snakemake.input.get("fq2") or snakemake.input[1]
 if fq2:
     in2 = f"in2='{fq2}'"
 else:
