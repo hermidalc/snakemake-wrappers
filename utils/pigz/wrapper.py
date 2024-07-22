075b53d522521c2769826d465f64276c4955e9df
Leandro Hermida <softdev@leandrohermida.com>
2024-07-22 10:18:25 -0400

    Merge to single pigz compress/decompress wrapper

--- utils/pigz/wrapper.py
+++ utils/pigz/wrapper.py
@@ -6,19 +6,24 @@ from snakemake.shell import shell
 
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
+decompress = snakemake.params.get("decompress", False)
+if decompress:
+    decompress = "--decompress"
+
 file_type = (
     "--zip"
     if snakemake.input[0].endswith((".zip", ".ZIP"))
-    else "--zlib"
-    if snakemake.input[0].endswith((".zz", ".ZZ"))
-    else ""
+    else "--zlib" if snakemake.input[0].endswith((".zz", ".ZZ")) else ""
 )
+
+extra = snakemake.params.get("extra", "")
 
 shell(
     "pigz"
     " --processes {snakemake.threads}"
+    " {decompress}"
     " {file_type}"
-    " --decompress"
+    " {extra}"
     " --stdout {snakemake.input[0]} > {snakemake.output[0]}"
     " {log}"
 )
