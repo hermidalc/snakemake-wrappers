ec70999e13c0150b425aa057b573ebc9ef8c98c7
Leandro Hermida <softdev@leandrohermida.com>
2024-10-29 20:50:40 -0400

    Better shell cmd and logging

--- bio/bbmap/readlength/wrapper.py
+++ bio/bbmap/readlength/wrapper.py
@@ -1,10 +1,12 @@
 __author__ = "Leandro C. Hermida"
 __email__ = "hermidalc@pitt.edu"
-__license__ = "BSD 3-Clause"
+__license__ = "MIT"
+
+import re
 
 from snakemake.shell import shell
 
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+log = snakemake.log_fmt_shell(stdout=True, stderr=True, append=True)
 
 fq1 = snakemake.input.get("fq1") or snakemake.input[0]
 assert fq1 is not None, "input: fq1 is a required named or positional input parameter"
@@ -15,4 +17,18 @@ if fq2:
 else:
     in2 = ""
 
-shell("readlength.sh in='{fq1}' {in2} bin=1 out='{snakemake.output}' {log}")
+extra = snakemake.params.get("extra", "")
+
+shellcmd = (
+    f"JAVA_OPTS='-XX:ActiveProcessorCount={snakemake.threads}' readlength.sh"
+    f" in='{fq1}'"
+    f" {in2}"
+    f" bin=1"
+    f" out='{snakemake.output}'"
+    f" {extra}"
+    f" {log}"
+)
+shellcmd = re.sub(r"\s+", " ", shellcmd)
+with open(snakemake.log[0], "wt") as log_fh:
+    log_fh.write(f"{shellcmd}\n")
+shell(shellcmd)
